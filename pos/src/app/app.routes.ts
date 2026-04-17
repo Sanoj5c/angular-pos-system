8259494a42208e873b58ddde86b62a59202a6e01
Sanoj5c <sanojdayarathna5cc@gmail.com>
2026-04-17 23:23:36 +0530

    creating header

--- pos/src/app/app.routes.ts
+++ pos/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'home', loadComponent:()=> import('./Pages/home-page/home-page.component').then(ob=>ob.HomePageComponent), title:' POS | HOME'},
+];
