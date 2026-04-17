4ac3c27b43278cd07f1528e58692692332fea6f0
Sanoj5c <sanojdayarathna5cc@gmail.com>
2026-04-17 21:39:42 +0530

    adding twak ai

--- pos/src/app/app.component.ts
+++ pos/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainFooterComponent } from "./core/main-footer/main-footer.component";
+import { MainHeaderComponent } from "./core/main-header/main-header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MainFooterComponent, MainHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
